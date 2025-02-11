10f220a4ba671d2e7eed32903b83af38e599b741
sheikh abdullah <sheikhabdullah.aps@gmail.com>
2025-02-12 02:02:24 +0530

    private dashboard created

---
+++ codefast-saas/app/dashboard/page.js
@@ -0,0 +1,8 @@
+
+export default function Dashboard() {
+  return (
+    <main>
+      <h1>Private Dashboard</h1> 
+    </main>
+  );
+}
--- codefast-saas/app/page.js
+++ codefast-saas/app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Get Started</button>
+      <h1>Collect customer feedback to build better products.</h1>
+      <div>
+        Create feedback board in minutes, prioritize features and build features your customer will love.
+      </div>
+      {/* <button className="btn">Get Started</button> */}
     </main>
   );
 }
